dd8f29716c7ea1f607e444f04112ea86e938c66d
Sucharita Das <sd686@afsconnect2.njit.edu>
2017-12-17 22:00:16 -0500

    session checking on login added

--- controllers/homepageController.php
+++ controllers/homepageController.php
@@ -4,11 +4,11 @@ class homepageController extends http\controller
 {
     public static function show()
     {
-//You could get fancy with the homepage and check for the userID in the session and hide/show the login / registration links when no session
-//If there is a session then you should show the user profile link
-//the template is an HTML page with PHP inserted in it.  just put an if/else statement to check for the session and show correct links
-        $templateData['site_name'] = 'mysite';
-        self::getTemplate('homepage', $templateData);
+        if(key_exists('userID',$_SESSION)) {
+            header('Location: index.php?page=tasks&action=allOneUser&id='.$_SESSION["userID"]);
+        } else {
+            self::getTemplate('homepage');
+        }
     }
 }
 ?>
